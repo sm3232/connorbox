16e200f87552dcdafa86c904f79f358b7616c163
sm3232 <connor.hill@mymail.champlain.edu>
2024-03-29 16:56:23 -0400

    gnome

---
+++ visualizations/gnomesort/script.js
@@ -0,0 +1,21 @@
+import * as module from "../../scripts/script.js";
+Object.entries(module).forEach(([name, exported]) => window[name] = exported);
+const sort = () => {
+    let pos = 0;
+    while(pos < nums.length){
+        steps.push({f: grab, p: [nums[pos]]});
+        if(pos === 0 || nums[pos].number >= nums[pos - 1].number){
+            steps.push({f: release, p: [nums[pos]]});
+            pos++;
+        } else {
+            steps.push({f: grab, p: [nums[pos - 1]]});
+            steps.push({f: swap, p: [nums[pos], nums[pos - 1]]});
+            steps.push({f: release, p: [nums[pos], nums[pos - 1]]});
+            let temp = nums[pos];
+            nums[pos] = nums[pos - 1];
+            nums[pos - 1] = temp;
+            pos--;
+        }
+    }
+}
+start(); resize(); sort(); animate(); update();
